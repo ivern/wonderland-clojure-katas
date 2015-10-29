ca7afd295293942bca9c50133b50fe6808f33539
Javier Fernandez-Ivern <javier.ivern@gmail.com>
2015-10-29 00:08:36 -0500

    Solved wonderland-number

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,10 @@
 (ns wonderland-number.finder)
 
+(defn check
+  ([n] (and (check n 2) (check n 3) (check n 4) (check n 5) (check n 6)))
+  ([n m] (= (set (str n)) (set (str (* n m))))))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (->> (range 100000 1000000)
+       (filter check)
+       (first)))
