9f627b0fa6d5e94d4a94b82fbcdb2766613f5f45
Javier Fernandez-Ivern <javier.ivern@gmail.com>
2015-10-29 22:04:28 -0500

    Solved magic-square

--- magic-square/src/magic_square/puzzle.clj
+++ magic-square/src/magic_square/puzzle.clj
@@ -1,8 +1,24 @@
-(ns magic-square.puzzle)
+(ns magic-square.puzzle
+  (:require [clojure.math.combinatorics :as combo]))
 
 (def values [1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0])
 
+(defn ->matrix [v]
+  (->> v (partition 3) (map vec) (into [])))
+
+(defn magic? [values]
+  (let [rows (->matrix values)
+        cols (apply map vector rows)
+        d1sum (+ (values 0) (values 4) (values 8))
+        d2sum (+ (values 2) (values 4) (values 6))
+        rsum (apply vector (map #(reduce + %) rows))
+        csum (apply vector (map #(reduce + %) cols))]
+    (->> (vector d1sum)
+         (concat rsum csum)
+         (every? #(= % d2sum)))))
+
 (defn magic-square [values]
-  [[1.0 1.5 2.0]
-   [2.5 3.0 3.5]
-   [4.0 4.5 5.0]])
+  (->> (combo/permutations values)
+       (filter magic?)
+       (first)
+       (->matrix)))
