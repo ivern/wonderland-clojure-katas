333502dac5961c2ef6d19a2b70ffe9bcb7284ffd
Javier Fernandez-Ivern <javier.ivern@gmail.com>
2016-07-23 14:13:30 -0500

    Cleanup

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -3,21 +3,28 @@
   (:import (clojure.lang PersistentQueue)))
 
 (def start-pos [[#{:fox :goose :corn :you} #{:boat} #{}]])
-(def end-state [#{} #{:boat} #{:fox :goose :corn :you}])
+
+(defn goal-pos? [pos]
+  (= (first pos) [#{} #{:boat} #{:fox :goose :corn :you}]))
 
 (defn options [place]
   (let [stuff (filter #{:fox :goose :corn} place)]
     (cons #{:you} (map #(hash-set :you %) stuff))))
 
-(defn moves [state]
-  (let [[near boat far] state
+(defn moves [pos]
+  (let [[near boat far] (first pos)
         outof set/difference]
     (cond
-      (:you near) (map #(vector (outof near %) (into boat %) far) (options near))
-      (:you boat) (let [to-near (map #(vector (into near %) (outof boat %) far) (options boat))
-                        to-far (map #(vector near (outof boat %) (into far %)) (options boat))]
-                    (concat to-near to-far))
-      (:you far) (map #(vector near (into boat %) (outof far %)) (options far)))))
+      (:you near)
+      (map #(vector (outof near %) (into boat %) far) (options near))
+
+      (:you boat)
+      (let [to-near (map #(vector (into near %) (outof boat %) far) (options boat))
+            to-far (map #(vector near (outof boat %) (into far %)) (options boat))]
+        (concat to-near to-far))
+
+      (:you far)
+      (map #(vector near (into boat %) (outof far %)) (options far)))))
 
 (defn valid? [state]
   (let [boat (nth state 1)]
@@ -31,10 +38,9 @@
   (loop [fringe (cons start-pos PersistentQueue/EMPTY)
          seen (hash-set (first start-pos))]
     (when-let [next-pos (first fringe)]
-      (let [next-state (first next-pos)]
-        (if (= next-state end-state)
-          (vec (reverse (map #(vec (map vec %)) next-pos)))
-          (let [valid-moves (filter #(and (not (contains? seen %)) (valid? %))
-                                    (moves next-state))]
-            (recur (into (rest fringe) (map #(cons % next-pos) valid-moves))
-                   (into seen valid-moves))))))))
+      (if (goal-pos? next-pos)
+        (vec (reverse (map #(vec (map vec %)) next-pos)))
+        (let [valid-moves (filter #(and (not (contains? seen %)) (valid? %))
+                                  (moves next-pos))]
+          (recur (into (rest fringe) (map #(cons % next-pos) valid-moves))
+                 (into seen valid-moves)))))))
